995c56aa7c74735f8344672803fd0d5bcd3760ce
KimTaeuk <kimtae@stitch-mac.local>
2017-07-25 11:43:26 +0900

    Fixed bug, Add argv

--- fixed_packet_parser.cpp
+++ fixed_packet_parser.cpp
@@ -6,10 +6,16 @@
 
 int count = 0;
 
-void print_pack(libnet_ethernet_hdr *eth, libnet_ipv4_hdr *ip, libnet_tcp_hdr *tcp)
+void print_pack(libnet_ethernet_hdr *eth, libnet_ipv4_hdr *ip, libnet_tcp_hdr *tcp, const unsigned char *p)
 {
     char src_ip[16];
     char dst_ip[16];
+
+	int size_ip = (ip->ip_hl) & 0x0f;
+	int size_tcp = ((tcp->th_x2 & 0xf0) >> 4)*4;
+	int size_tcpdat = ntohs(ip->ip_len) - (size_ip + size_tcp);
+	
+	char *payload = (char *)(p + LIBNET_ETH_H + size_ip + size_tcp);
 
     printf("NO.%d\n",count);
     printf("============[Packet Info]===========\n");
@@ -21,6 +27,8 @@ void print_pack(libnet_ethernet_hdr *eth, libnet_ipv4_hdr *ip, libnet_tcp_hdr *t
     printf("------------------------------------\n");
     printf("| src port     : %6d            |\n",ntohs(tcp->th_sport));
     printf("| dst port     : %6d            |\n",ntohs(tcp->th_dport));
+    printf("--------------[TCP_DATA]-----------\n");
+	DumpHex(payload, size_tcpdat);
     printf("====================================\n\n\n");
     count++;
 }
@@ -32,44 +40,52 @@ void packet_viewer(const unsigned char *p)
 	libnet_tcp_hdr *tcp_h;	
 	int eth_len = sizeof(*eth_h);
 	int ip_len = sizeof(*ip_h);
-	char *payload;               
-	int size_ip;
-	int size_tcp;
-	int size_payload;
 
 	eth_h = (libnet_ethernet_hdr *) p;
-
 	ip_h = (libnet_ipv4_hdr *) (p+eth_len);
-	size_ip = (ip_h->ip_hl) & 0x0f;
 	
-	if(ip_h->ip_p == IPPROTO_TCP)
+	if(ip_h->ip_p == IPPROTO_TCP){
 		tcp_h = (libnet_tcp_hdr *) (p+eth_len+ip_len);	
-		size_tcp = ((tcp_h->th_x2 & 0xf0) >> 4)*4;
-
-		payload = (char *)(p + LIBNET_ETH_H + size_ip + size_tcp);
-		size_payload = ntohs(ip_h->ip_len) - (size_ip + size_tcp);
-
-		print_pack(eth_h, ip_h, tcp_h);
-		DumpHex(payload, size_payload);
+		print_pack(eth_h, ip_h, tcp_h, p);
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	const uint8_t *packet;
-	char errbuf[PCAP_ERRBUF_SIZE];   /* Error string */
+	char errbuf[PCAP_ERRBUF_SIZE];
 	struct pcap_pkthdr *header;
 	pcap_t *handle;
+	uint8_t res;
 	
-	if((handle = pcap_open_live("enp0s5", 2048, 1, 1024, errbuf)) == NULL){
+	if (argc != 2){
+		printf("[!] Usage : %s [DEVICE]\n", argv[0]);
+		return 0;
+	}
+
+	if((handle = pcap_open_live(argv[1], 2048, 1, 1024, errbuf)) == NULL){
 		printf("[!] Device open Error!!!\n"); 
 		perror(errbuf);
 		printf("[!] EXIT process\n");
 		exit(0);;
 	}
 	
+	/*	
 	while(pcap_next_ex(handle, &header, &packet)){
 		packet_viewer(packet);
 	}
+	*/
 
-	reture 0;
+	while(1){
+		res = pcap_next_ex(handle, &header, &packet);
+		if (res == 0 || packet == NULL)
+			continue;
+		if (res == -1 || res == -2){
+			printf("[!] EXIT process\n");
+			break; 
+		}
+		packet_viewer(packet);
+	}
+
+	return 0;
 }
